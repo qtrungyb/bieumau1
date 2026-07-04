1f0328c58fe3390b58239c0afcd78039a10d759a
trung <qtrungyb@gmail.com>
2026-07-04 07:21:54 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'bieumau-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Cài đặt Service Worker và lưu cache các file tĩnh
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Trả về dữ liệu từ cache nếu có, nếu không thì tải từ mạng
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
